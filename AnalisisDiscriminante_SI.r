ffad14c9b312883168383add51b4f023ff171de5
Felipe Bravo <fbravo@pvs.uva.es>
2020-10-16 00:14:08 +0200

    Rename AnalisisDiscriminante_SI.R to AnalisisDiscriminante_SI.r

--- AnalisisDiscriminante_SI.r
+++ AnalisisDiscriminante_SI.r
@@ -1,9 +1,9 @@
 # Felipe Bravo (Univesidad de Valladolid), fbravo [at] pvs.uva.es
-# Universidad TÈcnica del ChocÛ, 2020
-# An·lisis discriminante
+# Universidad T√©cnica del Choc√≥, 2020
+# An√°lisis discriminante
 
 # En este ejemplo aprenderemos a
-  # Desarrollar un an·lisis discriminante para clasificar estaciones forestales
+  # Desarrollar un an√°lisis discriminante para clasificar estaciones forestales
 
 
 ###############
@@ -18,19 +18,19 @@ suelo <- read.table("DATOS2.txt", header=TRUE, sep="\t", dec=".")
 
 head(suelo)
 
-Realizar el an·lisis discriminante
+Realizar el an√°lisis discriminante
 
 install.packages(MASS)
 library(MASS)
 
 disc <- lda(SI4 ~ ., data=suelo)
-# An·lisis discriminante lineal
+# An√°lisis discriminante lineal
 
 Disc
 # muestra el resultado
 
 plot(disc)
-# dibuja la dispersiÛn de datos frente a las funciones lineales #definidas (en este caso 3)
+# dibuja la dispersi√≥n de datos frente a las funciones lineales #definidas (en este caso 3)
 
 clas <- predict(disc,suelo[,-1])
 plot(clas$x,pch=as.character(suelo[,1]),col=as.numeric(clas$class)+1)
