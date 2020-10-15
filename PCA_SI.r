4b66e2c80673331a013016328534a7e4b5bf64b5
Felipe Bravo <fbravo@pvs.uva.es>
2020-10-16 00:14:31 +0200

    Rename PCA_SI.R to PCA_SI.r

--- PCA_SI.r
+++ PCA_SI.r
@@ -1,6 +1,6 @@
 # Felipe Bravo (Univesidad de Valladolid), fbravo [at] pvs.uva.es
-# Universidad TÈcnica del ChocÛ, 2020
-# PCA - An·lisis de Componentes Principales
+# Universidad T√©cnica del Choc√≥, 2020
+# PCA - An√°lisis de Componentes Principales
 
 # En este ejemplo aprenderemos a
   # Reducir la dimensionalidad de una base de datos mediante PCA
@@ -23,12 +23,12 @@ head(suelo)
 
 pc <- princomp(suelo[,-1],cor=TRUE)
 
-plot (pc, type="l") #gr·fico con la varianza que recoge cada componente
+plot (pc, type="l") #gr√°fico con la varianza que recoge cada componente
 
 pc$loadings # peso de cada componente principal
-pc$scores # valor de cada componente principal para cada observaciÛn
+pc$scores # valor de cada componente principal para cada observaci√≥n
 
-biplot(pc)#gr·fico de componentes principales con los pesos de cada variable
+biplot(pc)#gr√°fico de componentes principales con los pesos de cada variable
 
 plot(pc$scores[,1],pc$scores[,2])#Dibuja las observaciones sobre los dos primeros ejes
-text(pc$scores[,1],pc$scores[,2],suelo[,1],pos=4) # AÒade la etiqueta del Õndice de sitio
+text(pc$scores[,1],pc$scores[,2],suelo[,1],pos=4) # A√±ade la etiqueta del √çndice de sitio
